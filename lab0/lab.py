0aafb5e46beee8211ff6516704b307bfe54b5c6c
Vivek Verma <vivek.verma662ha@gmail.com>
2021-01-07 11:45:28 +0530

    docstrings added lab0

--- lab0/lab.py
+++ lab0/lab.py
@@ -1,80 +1,162 @@
 # No Imports Allowed!
 
 
+import io
+import struct
+import wave
+
+
 def backwards(sound):
-    r_sound = sound.copy()
-    r_sound["left"] = []
-    r_sound["right"] = []
+    """
+    Reverse given sound.
 
-    r_sound["left"] = [l for l in reversed(sound["left"])]
-    r_sound["right"] = [r for r in reversed(sound["right"])]
+    Args:
+        sound (dict): given sound of interest.
 
-    return r_sound
+    Returns:
+        (dict)
+    """
+
+    backward_sound = sound.copy()
+
+    backward_sound["left"] = list(reversed(sound["left"]))
+    backward_sound["right"] = list(reversed(sound["right"]))
+
+    return backward_sound
 
 
 def mix(sound1, sound2, p):
+    """
+    Mix two sounds together.
+
+    Args:
+        sound1 (dict): first sound
+        sound2 (dict): second sound
+        p (float): proportion of first sound to add; must lie between 0 and 1;
+            second sound will be in (1-p) proportion.
+
+    Returns:
+        (dict)
+    """
+
     if sound1["rate"] != sound2["rate"]:
         return None
 
     mixed = {"rate": sound1["rate"]}
 
-    mixed["left"] = [p * l1 + (1 - p) * l2 for l1, l2 in zip(sound1["left"], sound2["left"])]
-    mixed["right"] = [p * r1 + (1 - p) * r2 for r1, r2 in zip(sound1["right"], sound2["right"])]
+    left = zip(sound1["left"], sound2["left"])
+    right = zip(sound1["right"], sound2["right"])
+    mixed["left"] = [p * l1 + (1 - p) * l2 for l1, l2 in left]
+    mixed["right"] = [p * r1 + (1 - p) * r2 for r1, r2 in right]
 
     return mixed
 
 
-def echo_samples(samples, sample_delay, scale ):
+def echo_filter(samples, sample_delay, scale):
+    """
+    Apply Echo filter to given list of samples.
 
+    Args:
+        samples (list): list of samples to apply filter on.
+        sample_delay (int): amount by which each sample should be offset.
+        scale (float): amount by which each sample should be scaled.
+
+    Returns:
+        (list)
+    """
 
     new_samples = samples.copy()
 
-    st_idx = len(samples)
+    # index at which first echoed sample will append.
+    start_idx = len(samples)
 
-    for i in range(sample_delay):
-        scale_idx = st_idx - sample_delay
+    for _ in range(sample_delay):
+        # index whose element will scaled and inserted at start_idx
+        scale_idx = start_idx - sample_delay
+
         if scale_idx < 0:
-            new_samples.append( 0 )
+            new_samples.append(0)
         else:
-            new_samples.append( scale * samples[ scale_idx ]  )
-        st_idx += 1
+            new_samples.append(scale * samples[scale_idx])
+        start_idx += 1
+
     return new_samples
 
 
 def echo(sound, num_echos, delay, scale):
+    """
+    Apply Echo filter to given (stereo) sound.
+
+    Args:
+        sound (dict): representation of original sound.
+        num_echos (int): # additional coipies of sound to add.
+        delay (float): amount (seconds) by which each "echo" should be delayed.
+        scale (float): amount by which each echo's sample is scaled.
+
+    Returns:
+        (dict)
+    """
 
     sample_delay = round(delay * sound["rate"])
 
-    echoed = {"rate": sound["rate"] }
+    echoed = {"rate": sound["rate"]}
 
-    left = sound["left"]
-    right = sound["right"]
-    for num in range( num_echos ):
-        left = echo_samples(left, sample_delay, scale )
-        right = echo_samples(right, sample_delay, scale )
-    
-    echoed["left"] = left
-    echoed["right"] = right
+    left, right = sound["left"], sound["right"]
+
+    for _ in range(num_echos):
+        left = echo_filter(left, sample_delay, scale)
+        right = echo_filter(right, sample_delay, scale)
+
+    echoed["left"], echoed["right"] = left, right
 
     return echoed
-    
-def pan(sound):
-    
-    panned = {"rate": sound["rate"]}
-    left_scale = lambda x,i: x * ( 1 - (i / ( len(sound["left"]) - 1 )) )
-    right_scale = lambda x,i: x * ( i / (len(sound["right"]) - 1 ))
 
-    panned["left"] = [ left_scale(l, i) for i, l in enumerate(sound["left"]) ]
-    panned["right"] = [ right_scale(r, i) for i, r in enumerate(sound["right"])]
-    
+
+def pan(sound):
+    """
+    Pan the given sound.
+
+    Args:
+        sound (dict): requires sound to be stereo.
+
+    Returns:
+        (dict)
+    """
+
+    panned = {"rate": sound["rate"]}
+
+    def left_scale(sample, idx):
+        # start at full volume and end at zero volume
+
+        return sample * (1 - (idx / (len(sound["left"]) - 1)))
+
+    def right_scale(sample, idx):
+        # start at zero volume and end at full volume
+
+        return sample * (idx / (len(sound["right"]) - 1))
+
+    panned["left"] = [left_scale(s, i) for i, s in enumerate(sound["left"])]
+    panned["right"] = [right_scale(s, i) for i, s in enumerate(sound["right"])]
+
     return panned
 
 
-
 def remove_vocals(sound):
+    """
+    Remove vocals from sound.
+
+    Args:
+        sound (dict): given sound with vocals
+
+    Returns:
+        (dict)
+    """
+
     wo_vocal = {"rate": sound["rate"]}
 
-    wo_vocal["left"] = [l - r for l, r in zip(sound["left"], sound["right"])]
+    left_right = zip(sound["left"], sound["right"])
+
+    wo_vocal["left"] = [ls - rs for ls, rs in left_right]
     wo_vocal["right"] = wo_vocal["left"].copy()
 
     return wo_vocal
@@ -82,9 +164,6 @@ def remove_vocals(sound):
 # below are helper functions for converting back-and-forth between WAV files
 # and our internal dictionary representation for sounds
 
-import io
-import wave
-import struct
 
 def load_wav(filename):
     """
@@ -98,8 +177,10 @@ def load_wav(filename):
 
     left = []
     right = []
+
     for i in range(count):
         frame = f.readframes(1)
+
         if chan == 2:
             left.append(struct.unpack('<h', frame[:2])[0])
             right.append(struct.unpack('<h', frame[2:])[0])
@@ -124,6 +205,7 @@ def write_wav(sound, filename):
     outfile.setparams((2, 2, sound['rate'], 0, 'NONE', 'not compressed'))
 
     out = []
+
     for l, r in zip(sound['left'], sound['right']):
         l = int(max(-1, min(1, l)) * (2**15-1))
         r = int(max(-1, min(1, r)) * (2**15-1))
@@ -143,6 +225,9 @@ if __name__ == '__main__':
     # here is an example of loading a file (note that this is specified as
     # sounds/hello.wav, rather than just as hello.wav, to account for the
     # sound files being in a different directory than this file)
-    car = load_wav('sounds/car.wav')
 
-    write_wav(pan(car), 'sounds/car_pan.wav')
+    # car = load_wav('sounds/car.wav')
+
+    # write_wav(pan(car), 'sounds/car_pan.wav')
+
+    pass
